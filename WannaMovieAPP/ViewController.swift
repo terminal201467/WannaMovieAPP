ff49e9b2a630a92a50f0acae43cabeb326e55d91
MJ <terminal201467@gmail.com>
2022-01-11 16:50:06 +0800

    Initial Commit

---
+++ WannaMovieAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WannaMovieAPP
+//
+//  Created by Jhen Mu on 2022/1/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
